65aaace2a7314407421874c13e6e684b5a790668
crazy0629 <milan.jansen0629@gmail.com>
2024-01-30 22:37:02 +0900

    change: port 8000

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import "./database";
 
 const server = http.createServer(app);
 
-const port = app.get("port") || 8002; // Ensure 'port' is set in 'app'
+const port = app.get("port") || 8000; // Ensure 'port' is set in 'app'
 
 server.listen(port, () => {
   console.log(`>> Server is running on port ${port}`);
